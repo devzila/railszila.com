72e591cca92291beb5ccd4853c3e7fec368ec667
Nilay <nilayanand@gmail.com>
2019-05-07 19:53:46 +0530

    Convert to PHP

---
+++ inc/footer.php
@@ -0,0 +1,64 @@
+      <footer>
+         <div class="container">
+            <div class="row">
+               <div class="col-md-4 col-sm-6 col-12">
+                  <h3>Quick Navigation</h3>
+                  <ul class="footer-list">
+                     <li><a href="#">About us</a></li>
+                     <li><a href="#">Why Us?</a></li>
+                     <li><a href="#">Our Team</a></li>
+                     <li><a href="#">Careers</a></li>
+                     <li><a href="#">Development Workflow</a></li>
+                     <li><a href="#">Technology Stack</a></li>
+                     <li><a href="#">Agile Development Methodology</a></li>
+                  </ul>
+               </div>
+               <div class="col-md-4 col-sm-6 col-12">
+                  <h3>Our Services</h3>
+                  <ul class="footer-list">
+                     <li><a href="#">Ruby On Rails Consulting</a></li>
+                     <li><a href="#">Mobile App Development</a></li>
+                     <li><a href="#">Hire Ruby on Rails Developer</a></li>
+                     <li><a href="#">Product Development Services</a></li>
+                     <li><a href="#">Spree Commerce Development</a></li>
+                     <li><a href="#">DevOps Services</a></li>
+                     <li><a href="#">Ruby on Rails App Development</a></li>
+                  </ul>
+               </div>
+               <div class="col-md-4 col-sm-6 col-12">
+                  <h3>Contact</h3>
+                  <div class="mt-20">
+                     <p>SCO 96 Ground Floor,<br> Sector 5, Panchkula,<br> Haryana 134109</p>
+                     <ul class="social-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="footer-bar">
+               <p><span class="primary-color">Devzila</span> &copy; 2019. All Rights Reserved.</p>
+            </div>
+         </div>
+      </footer>
+      <a href="#" class="scroll-to-top"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/owl.carousel.js"></script>
+      <script src="js/navigation.js"></script>
+      <script src="js/navigation.fixed.js"></script>
+      <script src="js/wow.min.js"></script>
+      <script src="js/jquery.counterup.min.js"></script>
+      <script src="js/waypoints.min.js"></script>
+      <script src="js/tabs.min.js"></script>
+      <script src="js/jquery.mb.YTPlayer.min.js"></script>
+      <script src="js/swiper.min.js"></script>
+      <script src="js/isotope.pkgd.min.js"></script>
+      <script src="js/modernizr.js"></script>
+      <script src="js/map.js"></script>
+      <script src="js/main.js"></script>
+   </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <title>RailsZila</title>
+      <meta charset="UTF-8">
+      <link rel="shortcut icon" href="img/logos/logo-shortcut.png"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+      <link rel="stylesheet" type="text/css" href="css/icomoon.css">
+      <link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+      <link rel="stylesheet" type="text/css" href="css/slider.css">
+      <link rel="stylesheet" type="text/css" href="css/animate.css">
+      <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+      <link rel="stylesheet" type="text/css" href="css/default.css">
+      <link rel="stylesheet" type="text/css" href="css/styles.css" id="colors">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+   </head>
+   <body>
+      <div id="preloader">
+         <div class="row loader">
+            <div class="loader-icon"></div>
+         </div>
+      </div>
+      <header>
+         <nav id="navigation4" class="container navigation">
+            <div class="nav-header">
+               <a class="nav-brand" href="index.html"><img src="img/logos/logo.png" class="main-logo" alt="logo" id="main_logo"></a>
+               <div class="nav-toggle"></div>
+            </div>
+            <div class="nav-menus-wrapper">
+               <ul class="nav-menu align-to-right">
+                  <li>
+                     <a href="#">Home</a>					
+                  </li>
+                  <li>
+                     <a href="#">Company</a>
+                     <div class="megamenu-panel nav-submenu">
+                        <div class="megamenu-lists">
+                           <ul class="megamenu-list list-col-2">
+                              <li><a href="#">About Us</a></li>
+                              <li><a href="#">Why Us</a></li>
+                              <li><a href="#">Our Team</a></li>
+                              <li><a href="#">Career</a></li>
+                           </ul>
+                           <ul class="megamenu-list list-col-2">
+                              <li><a href="#">Our Development Flow</a></li>
+                              <li><a href="#">Technology Stack</a></li>
+                              <li><a href="#">Agile Web Development</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                  </li>
+                  <li>
+                     <a href="#">Services</a>
+                      <div class="megamenu-panel nav-submenu">
+                        <div class="megamenu-lists">
+                           <ul class="megamenu-list list-col-2">
+                              <li><a href="#">Ruby on Rails Consulting</a></li>
+                              <li><a href="#">Mobile App Development</a></li>
+                              <li><a href="#">Hire Ruby on Rails Developer</a></li>
+                              <li><a href="#">Product Development Services</a></li>
+                              <li><a href="#">Spree eCommerce Development</a></li>
+                              <li><a href="#">Devops Services</a></li>
+                           </ul>
+                           <ul class="megamenu-list list-col-2">
+                              <li><a href="#">Custom App Development</a></li>
+                              <li><a href="#">Cloud Hosting Development</a></li>
+                              <li><a href="#">API Driven Development</a></li>
+                              <li><a href="#">App Support and Maintenance</a></li>
+                              <li><a href="#">SaaS App Development</a></li>
+                              <li><a href="#">Rails Upgrade Service</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                  </li>
+                  <li>
+                     <a href="#">Projects</a>
+                  </li>
+                  <li>
+                     <a href="#">Case Studies</a>
+                     <div class="megamenu-panel nav-submenu">
+                        <div class="megamenu-lists">
+                           <ul class="megamenu-list list-col-2">
+                              <li><a href="#"><i class="fa fa-graduation-cap"></i> E-Learning Portal</a></li>
+                              <li><a href="#"><i class="fa fa-users"></i>Audience Measurement System</a></li>
+                              <li><a href="#"><i class="fa fa-laptop"></i>Educational Gaming Portal</a></li>
+                              <li><a href="#"><i class="fa fa-universal-access"></i>Health Care SaaS App</a></li>
+                              <li><a href="#"><i class="fa fa-male"></i>Fitness App</a></li>
+                              <li><a href="#"><i class="fa fa-arrows"></i>Integrated Advertising Platform</a></li>
+                              <li><a href="#"><i class="fa fa-shopping-bag"></i>Marketing Service App</a></li>
+                              <li><a href="#"><i class="fa fa-cart-plus"></i>Spree Ecommerce Store</a></li>
+                              <li><a href="#"><i class="fa fa-recycle"></i>Workflow Management System</a></li>
+                              <li><a href="#"><i class="fa fa-code"></i>Ecommerce CMS</a></li>
+                              <li><a href="#"><i class="fa fa-flask"></i>Inventory Management Solution</a></li>
+                              <li><a href="#"><i class="fa fa-database"></i>3D Model Creation</a></li>
+                           </ul>
+
+                           <ul class="megamenu-list list-col-2">
+                              <li><a href="#"><i class="fa fa-university"></i>University Survey App</a></li>
+                              <li><a href="#"><i class="fa fa-desktop"></i>Web Portal Development</a></li>
+                              <li><a href="#"><i class="fa fa-database"></i>Data Center Automation</a></li>
+                              <li><a href="#"><i class="fa fa-credit-card-alt"></i>Banking App</a></li>
+                              <li><a href="#"><i class="fa fa-file-text-o"></i>Patients Reporting System</a></li>
+                              <li><a href="#"><i class="fa fa-lock"></i>IT Security- Communications</a></li>
+                              <li><a href="#"><i class="fa fa-video-camera"></i>Media App</a></li>
+                              <li><a href="#"><i class="fa fa-play-circle-o"></i>Video CMS</a></li>
+                              <li><a href="#"><i class="fa fa-line-chart"></i>Practice Benchmarking</a></li>
+                              <li><a href="#"><i class="fa fa-cutlery"></i>Food Delivery Marketplace</a></li>
+                              <li><a href="#"><i class="fa fa-desktop"></i>SaaS Application</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                  <li>
+                     <a href="#">Blog</a>
+                  </li>
+                  <li>
+                     <a href="#">Contact</a>                     
+                  </li>
+               </ul>
+            </div>
+         </nav>
+      </header>
